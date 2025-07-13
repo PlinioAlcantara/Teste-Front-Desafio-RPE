2302c4ef3870cb6493dc053189567c1c3b5fded7
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-07-13 18:10:57 -0300

    criando o userCliente e aplicando a estilização

---
+++ gerenciador-clientes/src/hooks/userClientes.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export default function useClientes() {
+  const [clientes, setClientes] = useState([]);
+
+  const buscarClientes = () => {
+    fetch("http://localhost:8081/clientes")
+      .then(res => res.json())
+      .then(data => setClientes(data))
+      .catch(err => console.error("Erro ao buscar clientes:", err));
+  };
+
+  useEffect(() => {
+    buscarClientes();
+  }, []);
+
+  return { clientes, setClientes, buscarClientes };
+}
